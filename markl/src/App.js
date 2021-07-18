52351e016aa7cacc7ef61840b40e96d34a12f18f
Mark Laham <marklaham@gmail.com>
2021-07-17 21:03:07 -0400

    added Header and Footer

--- markl/src/App.js
+++ markl/src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer'; 
 import PortfolioContainer from "./components/PortfolioContainer";
 
-const App = () => <PortfolioContainer />;
+class App extends Component {
+    render() {
+      return (
+        <div>
+          <Header />
+          <PortfolioContainer/>
+          <Footer />
+          
+        </div>
+      );
+    }
+  }
 
 export default App;
